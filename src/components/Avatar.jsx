cbddde77cc42444fe1b5cf70dd29bdf59cad72eb
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-10-14 16:41:01 -0300

    :sparkles: feat: create Avatar and Experience components

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,20 @@
+import { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+export const Avatar = ({ ...props }) => {
+  const group = useRef();
+  const { nodes } = useGLTF("models/Armature.glb");
+
+  console.log(nodes);
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group name="Scene">
+        <group name="Armature" rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+          <primitive object={nodes.mixamorigHips} />
+        </group>
+      </group>
+    </group>
+  );
+};
+
+useGLTF.preload("/Armature.glb");
