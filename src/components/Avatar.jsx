67eac1c757ee8c40d1605b3e45da35fade95a18a
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-10-16 11:26:26 -0300

    :sparkles: feat: add idle animation to avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,14 +1,22 @@
 import { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
+import { useAnimations, useFBX, useGLTF } from "@react-three/drei";
 import { useConfiguratorStore } from "../store";
 import { Suspense } from "react";
 import { Asset } from "./Asset";
 import { pb } from "../store";
+import { useEffect } from "react";
 
 export const Avatar = ({ ...props }) => {
   const group = useRef();
   const { nodes } = useGLTF("models/Armature.glb");
+  const { animations } = useFBX("models/Idle.fbx");
   const customization = useConfiguratorStore((state) => state.customization);
+
+  const { actions } = useAnimations(animations, group);
+
+  useEffect(() => {
+    actions["mixamo.com"]?.play();
+  }, [actions]);
 
   return (
     <group ref={group} {...props} dispose={null}>
